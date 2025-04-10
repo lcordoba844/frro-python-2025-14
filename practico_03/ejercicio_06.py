26e27b0bce8754e35af27944f77f53005c588398
pecorarolucio <luciopecoraro2002@gmail.com>
2025-04-10 11:56:00 -0300

    Agrego eercicio 6 de practica 3

--- practico_03/ejercicio_06.py
+++ practico_03/ejercicio_06.py
@@ -17,6 +17,14 @@ class Article:
     # NO MODIFICAR - FIN
 
     # Completar
+    def __eq__(self, other: Article) -> bool:
+        return self.name == other.name
+    def __hash__(self) -> int:
+        return hash(self.name)
+    def __str__(self) -> str:
+        return self.name
+    def __repr__(self) -> str:
+        return f"Article('{self.name}')"    
 
 
 # NO MODIFICAR - INICIO
@@ -50,7 +58,14 @@ class ShoppingCart:
     # NO MODIFICAR - FIN
 
     # Completar
-
+    def __eq__(self, other: ShoppingCart) -> bool:
+        return set(self.articles) == set(other.articles) 
+    def __str__(self) -> str:
+        return str([str(i) for i in self.articles])
+    def __repr__(self) -> str:
+        return f"ShoppingCart({[art for art in self.articles]})"
+    def __add__(self, other):
+        return type(self)(self.articles + other.articles)
 
 # NO MODIFICAR - INICIO
 
